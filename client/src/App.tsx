a4aa7e30571f6629b69ab0fc935275f1057e59b1
SparrowMike <69448825+SparrowMike@users.noreply.github.com>
2021-08-06 15:27:11 +0800

    feat: 🎸 basic setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,10 @@
-import React, { Children, ReactElement, ReactNode } from 'react';
+import React from 'react';
 import './App.css';
 
-//* Conventional props
-//? SETUP BASED ON => https://www.youtube.com/watch?v=z8lDwLKthr8
-
-function Heading({title}: {title: string}){
-  return<h1>{title}</h1>
-}
-
-function HeadingWithContent({children}: {children: ReactNode}): ReactElement{
-  return <h1>{children}</h1>
-}
-
-function App() {
+function App() { 
   return (
     <div>
-      <Heading title="Hello Wolrd"></Heading>    
-      <HeadingWithContent>
-        <strong>Still here!</strong>
-      </HeadingWithContent>
+      <h1>Hello World</h1>
     </div>
   );
 }
