6326caac0f99632b178a4db8449d315e5c3ef14a
Sylvester <sylvesterhoe@gmail.com>
2021-08-19 03:50:00 +0800

    feat: 🎸 Added car images upload page
    Created form to handle car image uploading, submit does not work yet

--- client/src/components/editProfile/UploadCars.tsx
+++ client/src/components/editProfile/UploadCars.tsx
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from "react";
-import { makeStyles } from "@material-ui/core";
+import { makeStyles, Container, Button } from "@material-ui/core";
 import Cookies from "js-cookie";
+import { DropzoneArea } from "material-ui-dropzone";
+import { Formik } from "formik";
 
 const useStyles = makeStyles({
   form: {
     marginTop: 20,
+    textAlign: "center",
   },
   field: {
     marginTop: 10,
+  },
+  image: {
+    height: "280px",
   },
 });
 
@@ -27,12 +33,19 @@ interface currentCarImage {
 
 const UploadCars: React.FC = () => {
   const classes = useStyles();
+  const [image, setImage] = useState<any>();
   const [car, setCar] = useState<number>(0);
+  const [previewSource, setPreviewSource] = useState("");
   const [carImage, setCarImage] = useState<currentCarImage>();
+  const [initialValues, setinitialValues] = useState<FormValues>({
+    images_id: undefined,
+    cloudinary_id: undefined,
+    secure_url: undefined,
+    cars_id: undefined,
+  });
 
   useEffect(() => {
     const fetchSession = async () => {
-      // retrieve session ID from custom cookie
       const sidfromCookie = Cookies.get("cook");
       console.log("Session Id from Cookie: ", sidfromCookie);
 
@@ -66,6 +79,15 @@ const UploadCars: React.FC = () => {
       const imageData = await res.json();
       console.log(imageData[0]);
 
+      setinitialValues({
+        images_id: imageData[0]?.images_id,
+        cloudinary_id: imageData[0]?.cloudinary_id,
+        secure_url: imageData[0]?.secure_url,
+        cars_id: imageData[0]?.cars_id,
+      });
+
+      console.log("initialValues: ", initialValues);
+
       if (imageData.length > 0) {
         setCarImage(imageData[0]);
         console.log("this is carImage: ", imageData[0]);
@@ -74,16 +96,90 @@ const UploadCars: React.FC = () => {
       }
     };
     fetchImages();
-  }, [car]);
+  }, [car, initialValues?.images_id]);
+
+  const handleSubmit = async (formValue: FormValues) => {
+    const reader: any = new FileReader();
+    if (image) {
+      reader.readAsDataURL(image);
+      reader.onloadend = () => {
+        setPreviewSource(reader.result);
+      };
+      if (!previewSource) return;
+    } else {
+      setPreviewSource("");
+    }
+
+    const ImageURL = { secure_url: previewSource };
+    let merge = { ...initialValues, ...ImageURL };
+    console.log("this is merge: ", merge);
+
+    const updateUserAccount = async () => {
+      try {
+        const res = await fetch("/images/" + initialValues.images_id, {
+          method: "PUT",
+          body: JSON.stringify(merge),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        console.log(res);
+        alert("Image updated succesfully!");
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    updateUserAccount();
+  };
 
   return (
-    <div>
-      {carImage !== undefined ? (
-        <img src={carImage.secure_url} alt="" style={{ maxHeight: "280px" }} />
-      ) : (
-        <h1>Nothing</h1>
-      )}
-    </div>
+    <>
+      <div>
+        <Formik
+          initialValues={{
+            ...initialValues,
+          }}
+          onSubmit={handleSubmit}
+        >
+          {(formik) => (
+            <Container>
+              <form onSubmit={formik.handleSubmit} className={classes.form}>
+                <div className={classes.image}>
+                  {carImage !== undefined ? (
+                    <img
+                      src={carImage.secure_url}
+                      alt=""
+                      style={{ maxHeight: "280px" }}
+                    />
+                  ) : (
+                    <h1>Nothing was found</h1>
+                  )}
+                </div>
+                <div className={classes.field}>
+                  <DropzoneArea
+                    acceptedFiles={["image/*"]}
+                    dropzoneText={"Drag and drop an avatar here or click"}
+                    filesLimit={1}
+                    onChange={(files) => {
+                      setImage(files[0]);
+                    }}
+                  />
+                </div>
+                <Button
+                  className={classes.form}
+                  color="primary"
+                  variant="contained"
+                  type="submit"
+                  style={{ marginTop: 20 }}
+                >
+                  Submit
+                </Button>
+              </form>
+            </Container>
+          )}
+        </Formik>
+      </div>
+    </>
   );
 };
 
