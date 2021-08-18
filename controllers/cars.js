36c8a8c620c28b97c96f8f7f47fb1e1fd4addeda
sylvesterh <sylvesterhoe@gmail.com>
2021-08-18 14:09:24 +0800

    fix: 🐛 Pulled latest development changes in

--- controllers/cars.js
+++ controllers/cars.js
@@ -74,7 +74,7 @@ router.get("/:id", async (req, res) => {
 router.get("/imageusername/:username", async (req, res) => {
   try {
     const { username } = req.params;
-    const cars = await knexPg("cars").where("username", username);
+    const cars = await knexPg("cars").where("username", username.trim());
     res.status(200).json(cars[0]);
   } catch (error) {
     console.log(error.message);
