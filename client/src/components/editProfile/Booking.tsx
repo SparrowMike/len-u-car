dc9f2dcc8a50c4e5187c536b2385d52eca9085cc
SparrowMike <69448825+SparrowMike@users.noreply.github.com>
2021-08-19 12:07:33 +0800

    test: 💍 testing delete route, slight visual improvments on edit

--- client/src/components/editProfile/Booking.tsx
+++ client/src/components/editProfile/Booking.tsx
@@ -8,7 +8,7 @@ import { useState } from "react";
 import axios from "axios";
 import { useQuery } from "react-query";
 
-import { Card, Box } from "@material-ui/core";
+import { Card, CardActions } from "@material-ui/core";
 import CardContent from "@material-ui/core/CardContent";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
@@ -37,11 +37,6 @@ const useStyles = makeStyles((theme) => ({
     height: "100%",
     display: "flex",
     flexDirection: "column",
-    transition: ".3s ease-in-out",
-    "&:hover": {
-      transform: "scale(1.1)",
-      cursor: "pointer",
-    },
   },
   cardContent: {
     flexGrow: 1,
@@ -87,8 +82,8 @@ const ChangePassword: React.FC<IProps> = ({ user }) => {
   const [carsId, setCarsId] = useState<number>();
   const [eventId, setEventId] = useState<number>();
   const [modalOpen, setModalOpen] = React.useState(false);
+  const [modalDeleteOpen, setModalDeleteOpen] = React.useState(false);
 
-  console.log(user);
   const fetchEvents = async () => {
     const { data } = await axios.get(
       `/carRentalEvent/username/${user.username}`
@@ -122,14 +117,27 @@ const ChangePassword: React.FC<IProps> = ({ user }) => {
 
   const handleModalOpen = (e: any) => {
     setModalOpen(true);
-    console.log("WHATS IS GOING ON HEREREERERER!", e.card);
-
     setUsername(e.card.username);
     setCarsId(e.card.cars_id);
     setEventId(e.card.event_id);
   };
   const handleModalClose = () => {
     setModalOpen(false);
+    setModalDeleteOpen(false);
+  };
+
+  const handleModalDelete = (e: any) => {
+    setModalDeleteOpen(true);
+    setEventId(e.card.event_id);
+  };
+
+  const handleDelete = () => {
+    setModalDeleteOpen(false);
+    fetch(`/carRentalEvent/${eventId}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((res) => console.log(res));
   };
 
   return (
@@ -137,23 +145,29 @@ const ChangePassword: React.FC<IProps> = ({ user }) => {
       <Container className={classes.cardGrid} maxWidth="md">
         <Grid container spacing={4}>
           {data2?.map((card: any, index: any) => (
-            <Grid item key={index} xs={12} sm={6} md={4}>
+            <Grid item key={index} md={12}>
               <Card className={classes.card}>
                 <CardContent className={classes.cardContent}>
                   <Typography gutterBottom variant="h5" component="h2">
                     Car Booking {card.day}/{card.month}/{card.year}
                   </Typography>
                 </CardContent>
-                <Button
-                  onClick={() => handleModalOpen({ card })}
-                  variant="contained"
-                  color="primary"
-                >
-                  Review
-                </Button>
-                <Button variant="contained" color="primary">
-                  Delete
-                </Button>
+                <CardActions>
+                  <Button
+                    onClick={() => handleModalOpen({ card })}
+                    variant="contained"
+                    color="primary"
+                  >
+                    Review
+                  </Button>
+                  <Button
+                    onClick={() => handleModalDelete({ card })}
+                    variant="contained"
+                    color="primary"
+                  >
+                    Cancel
+                  </Button>
+                </CardActions>
               </Card>
             </Grid>
           ))}
@@ -180,7 +194,7 @@ const ChangePassword: React.FC<IProps> = ({ user }) => {
             >
               {(formik) => (
                 <form className={classes.form} onSubmit={formik.handleSubmit}>
-                  <Box component="fieldset" mb={5} borderColor="transparent">
+                  <Container>
                     <Typography component="legend">Rating</Typography>
                     <Rating
                       name="customized-empty"
@@ -190,27 +204,58 @@ const ChangePassword: React.FC<IProps> = ({ user }) => {
                       emptyIcon={<StarBorderIcon fontSize="inherit" />}
                       size="large"
                     />
-                  </Box>
-                  <Textfield
-                    className={classes.field}
-                    id="review"
-                    name="review"
-                    label="review"
-                    rows={5}
-                    cols={5}
-                    required
-                  />
-                  <Button
-                    color="primary"
-                    variant="contained"
-                    type="submit"
-                    style={{ marginTop: 10 }}
-                  >
-                    Submit
-                  </Button>
+                    <Textfield
+                      className={classes.field}
+                      id="review"
+                      name="review"
+                      label="review"
+                      rows={5}
+                      cols={5}
+                      required
+                    />
+                    <Button
+                      color="primary"
+                      variant="contained"
+                      type="submit"
+                      style={{ marginTop: 10 }}
+                    >
+                      Submit
+                    </Button>
+                  </Container>
                 </form>
               )}
             </Formik>
+          </div>
+        </Fade>
+      </Modal>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={modalDeleteOpen}
+        onClose={handleModalClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={modalDeleteOpen}>
+          <div className={classes.paper}>
+            <Container>
+              <Typography variant="h5" component="legend">
+                Proceed with booking cancellation?
+              </Typography>
+              <Button
+                onClick={handleDelete}
+                color="primary"
+                variant="contained"
+                type="submit"
+                style={{ margin: 10 }}
+              >
+                Confirm
+              </Button>
+            </Container>
           </div>
         </Fade>
       </Modal>
