b035e127ed68d0beb17508129122610667c8f15e
edangx100 <edangx20@outlook.com>
2021-08-19 14:01:00 +0800

    feat: 🎸 "CarPage.tsx" car review history displayed
    (1) "CarPage.tsx" car review history displayed (2) "CarRentalReview.js"
    controller add new GET route from joined tables
    'car_rental_review'-'car_rental_event'

--- server.js
+++ server.js
@@ -41,10 +41,10 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 //* ===========HEROKU DEPLOYMENT MIDDLEWARE==================
-app.use(express.static(path.join(__dirname, "./client/build")));
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build", "index.html"));
-});
+// app.use(express.static(path.join(__dirname, "./client/build")));
+// app.get("/*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/build", "index.html"));
+// });
 
 app.use(
   session({
